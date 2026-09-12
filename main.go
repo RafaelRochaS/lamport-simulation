a2a80d43e5e3a3df341490f3e9d595b060a8fffc
Rafael Rocha <rafaelbernardorocha57@gmail.com>
2026-09-12 19:04:26 -0300

    add: internal operation handler

--- main.go
+++ main.go
@@ -4,29 +4,47 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"sync"
+	"time"
 )
 
 type Message struct {
 	Operation  Operation `json:"operation"`
 	Sender     string    `json:"sender"`
 	Clock      int8      `json:"clock"`
-	DurationMs *int8     `json:"durationMs,omitempty"`
+	DurationMs *int16    `json:"durationMs,omitempty"`
 	Peers      *[]string `json:"peers,omitempty"`
 	ReturnPeer *string   `json:"returnPeer,omitempty"`
 }
 
 type Operation int8
 
+type LocalClock struct {
+	mu    sync.Mutex
+	value int8
+}
+
 const (
-	INTERNAL Operation = iota
-	EXTERNAL_SINGLE
-	EXTERNAL_MULTIPLE
-	HALT
+	Internal Operation = iota
+	ExternalSingle
+	ExternalMultiple
+	Halt
 )
 
-func operationHandler(w http.ResponseWriter, r *http.Request) {
+func (c *LocalClock) operationHandler(w http.ResponseWriter, r *http.Request) {
 	message := parseRequest(r)
 	log.Printf("Got request: %+v", message)
+
+	switch message.Operation {
+	case Internal:
+		handleInternalOperation(message, c)
+	case ExternalSingle:
+		handleExternalSingleOperation(message)
+	case ExternalMultiple:
+		handleExternalMultipleOperation(message)
+	case Halt:
+		handleHaltOperation(message)
+	}
 
 	w.WriteHeader(http.StatusOK)
 	_, err := w.Write([]byte("OK"))
@@ -47,10 +65,41 @@ func parseRequest(r *http.Request) Message {
 	return message
 }
 
+func handleInternalOperation(m Message, c *LocalClock) {
+	log.Println("Handling internal operation")
+
+	timer := *m.DurationMs
+	if timer == 0 {
+		timer = 1000
+	}
+
+	time.Sleep(time.Duration(timer) * time.Millisecond)
+
+	defer c.mu.Unlock()
+	c.mu.Lock()
+	c.value++
+
+	log.Println("Internal operation completed, current clock value: ", c.value)
+}
+
+func handleExternalSingleOperation(m Message) {
+	log.Println("Handling external single operation")
+}
+
+func handleExternalMultipleOperation(m Message) {
+	log.Println("Handling external multiple operation")
+}
+
+func handleHaltOperation(m Message) {
+	log.Println("Handling halting operation")
+}
+
 func main() {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/", operationHandler)
+	clock := &LocalClock{value: 0}
+
+	mux.HandleFunc("/", clock.operationHandler)
 
 	server := &http.Server{
 		Addr:    ":8080",
