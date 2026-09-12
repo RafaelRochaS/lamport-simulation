bb76657481d4705b95b92d7d6ddb6bc5c9923882
Rafael Rocha <rafaelbernardorocha57@gmail.com>
2026-09-12 17:59:00 -0300

    add: base server

--- main.go
+++ main.go
@@ -1,20 +1,64 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"log"
+	"net/http"
 )
 
-// TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
-func main() {
-	//TIP <p>Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined text
-	// to see how GoLand suggests fixing the warning.</p><p>Alternatively, if available, click the lightbulb to view possible fixes.</p>
-	s := "gopher"
-	fmt.Println("Hello and welcome, %s!", s)
+type Message struct {
+	Operation  Operation `json:"operation"`
+	Sender     string    `json:"sender"`
+	Clock      int8      `json:"clock"`
+	DurationMs *int8     `json:"durationMs,omitempty"`
+	Peers      *[]string `json:"peers,omitempty"`
+	ReturnPeer *string   `json:"returnPeer,omitempty"`
+}
 
-	for i := 1; i <= 5; i++ {
-		//TIP <p>To start your debugging session, right-click your code in the editor and select the Debug option.</p> <p>We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-		// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.</p>
-		fmt.Println("i =", 100/i)
+type Operation int8
+
+const (
+	INTERNAL Operation = iota
+	EXTERNAL_SINGLE
+	EXTERNAL_MULTIPLE
+	HALT
+)
+
+func operationHandler(w http.ResponseWriter, r *http.Request) {
+	message := parseRequest(r)
+	log.Printf("Got request: %+v", message)
+
+	w.WriteHeader(http.StatusOK)
+	_, err := w.Write([]byte("OK"))
+	if err != nil {
+		log.Fatalf("Error writing response: %v", err)
+	}
+}
+
+func parseRequest(r *http.Request) Message {
+	var message Message
+
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&message)
+	if err != nil {
+		log.Fatalf("Error parsing JSON: %v", err)
+	}
+
+	return message
+}
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", operationHandler)
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+
+	log.Println("Starting server on http://localhost:8080")
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatal(err)
 	}
 }
